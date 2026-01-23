1937f4de8114e2240bba00362838d0bc102eb798
ilzasania96 <sania.ilza96@gmail.com>
2026-01-22 19:06:25 -0500

    init sample_time_app

---
+++ Sample_time_app/run.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def get_time():
+    return datetime.now().strftime("%H:%M:%S")
+
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
